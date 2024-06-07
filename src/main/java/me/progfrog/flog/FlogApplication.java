bfb7e9eba757d8d1b02aace85659950f3ae8c993
happyprogfrog <happyprogfrog@gmail.com>
2024-06-07 15:50:26 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/flog/FlogApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.flog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/flog/FlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.flog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
