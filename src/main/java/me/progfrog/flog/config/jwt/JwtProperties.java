66bac759d36d04551a3285275f97e53cad981349
happyprogfrog <happyprogfrog@gmail.com>
2024-06-08 22:43:01 +0900

    feat: 토큰 제공자 추가

---
+++ src/main/java/me/progfrog/flog/config/jwt/JwtProperties.java
@@ -0,0 +1,16 @@
+package me.progfrog.flog.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("jwt")
+@Getter
+@Setter
+public class JwtProperties {
+
+    private String issuer;
+    private String secretKey;
+}
