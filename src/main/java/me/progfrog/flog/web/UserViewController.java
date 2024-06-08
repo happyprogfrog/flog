09816e6862155823ae384ed44d4509bd3f6926b9
happyprogfrog <happyprogfrog@gmail.com>
2024-06-08 11:47:38 +0900

    feat: UserApiController, UserViewController 추가

---
+++ src/main/java/me/progfrog/flog/web/UserViewController.java
@@ -0,0 +1,20 @@
+package me.progfrog.flog.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "users/login";
+    }
+
+    @GetMapping("/signup")
+    public String getSignupForm() {
+        return "users/signup";
+    }
+}
